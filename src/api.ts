390b8eea3d858d92a7542b0546283c1f1ddde94b
Jacek <jacek.kaczmarek@cornelsen.de>
2025-05-25 19:41:35 +0200

    fix: update imports

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { MessageDto, NewMessageDto } from './types';
+import { MessageDto, NewMessageDto } from './types.js';
 
 const API_BASE_URL = 'http://localhost:8080/v1/api/messages';
 
